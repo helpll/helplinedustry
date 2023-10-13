ae089363c14bbf04681b319aed942747205c341b
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-10-12 23:52:20 -0400

    Create dostuff.js

---
+++ scripts/dostuff.js
@@ -0,0 +1,12 @@
+// listen for the event where a unit is destroyed
+Events.on(UnitDestroyEvent, event => {
+  // display toast on top of screen when the unit was a player
+  if(event.unit.isPlayer()){
+    Vars.ui.hudfrag.showToast("Skill issue");
+  }
+})
+Events.on(ResearchEvent, event => {
+  
+    Vars.ui.hudfrag.showToast("Nice");
+  
+})
